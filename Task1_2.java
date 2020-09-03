12d0202f832f0f846542a314e1b239f02ca3d3ab
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-09-03 17:18:43 +0200

    Add files via upload

---
+++ Task1_2.java
@@ -0,0 +1,25 @@
+package stringOrStringBuilder;
+
+/*
+ * В строке вставить после каждого символа 'a' символ 'b'.
+ */
+
+public class Task1_2 {
+
+	public static void main(String[] args) {
+        String str = "alcohol beard head";
+        System.out.println(Task1_2.addBafterA(str));
+    }
+
+    private static String addBafterA(String str) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            stringBuilder.append(str.charAt(i));
+            if (str.charAt(i) == 'a') {
+                stringBuilder.append('b');
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
